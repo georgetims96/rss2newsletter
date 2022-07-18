567c48aaf27e6de164321e2356a8cf77417e864d
George Tims <george.tims@hotmail.com>
2022-07-17 22:53:17 -0400

    Added base user model

---
+++ feedaggregator/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+class Subscriber(AbstractBaseUser, PermissionsMixin):
+  username = models.CharField(max_length=50, unique=True)
+  email = models.EmailField(_('email address'), unique=True)
+  first_name = models.CharField(max_length=50, blank=False)
+  second_name = models.CharField(mac_length=50, blank=False)
+  is_staff = models.BooleanField(default=False)
+  is_active = models.BooleanField(defailt=False)
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
+
+class SubscriberManager(BaseUserManager):
+  def create_user(self, username, email, first_name, second_name, password, **others):
+    if not email:
+      raise ValueError("You must provide")
+
+    email = self.normalize_email(email)
+    user = self.model(username=username, first_name=first_name, second_name=second_name, **others)
+    user.set_password(password)
+    user.save()
+    return user
+  
+  def create_superuser(self, username, email, first_name, second_name, password, **others):
+    others.setdefault("is_staff", True)
+    others.setdefault("is_superuser", True)
+    others.setdefault("is_active", True)
