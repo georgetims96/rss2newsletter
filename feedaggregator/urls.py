cb81b8d5af77b3ff943ccd15bbd32bebd614db2e
George Tims <george.tims@hotmail.com>
2022-09-21 21:34:43 -0400

    Cleaned up URL paths

--- feedaggregator/urls.py
+++ feedaggregator/urls.py
@@ -4,6 +4,7 @@ from feedaggregator.views import EntryDetailView, FeedFormView, FeedDiscoverView
 app_name = 'feedaggregator'
 
 urlpatterns = [
+  path('', FeedDiscoverView.as_view(), name='home_view'),
   path('add/', FeedFormView.as_view(), name='add_feed'),
   path('discover/', FeedDiscoverView.as_view(), name='discover_feeds'),
   path('subscribe/<feed_pk>', FeedSubscribeView.as_view(), name="subscribe_feed"),
