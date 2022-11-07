f28aac04e76b74ba2f31154b0afc943e0514c6a7
George Tims <george.tims@hotmail.com>
2022-11-06 21:25:48 -0500

    Got bookmark POST working, but broke GET

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from feedaggregator.models import Bookmark
+from feedaggregator.models import Bookmark, Entry
+from users.models import Subscriber
 
 class BookmarkSerializer(serializers.ModelSerializer):
+    entry = serializers.PrimaryKeyRelatedField(many=False, queryset=Entry.objects.all())
+    subscriber = serializers.PrimaryKeyRelatedField(many=False, queryset=Subscriber.objects.all())
+
     class Meta:
         model = Bookmark
         fields = ['entry', 'subscriber']
         depth = 1
+    
+    def create(self, validated_data):
+        return Bookmark.objects.create(
+            entry=validated_data['entry'],
+            subscriber=validated_data['subscriber']
+        )
