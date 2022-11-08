33c2c0cc20b7c19fd0026bb6c3d48bfc3583b24f
George Tims <george.tims@hotmail.com>
2022-11-07 20:03:57 -0500

    Got POST AJAX working

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 from api.views import BookmarkViewSet
 
+
+
 router = routers.DefaultRouter()
 router.register(r'bookmarks', BookmarkViewSet)
 
