a9f2fcbe48119bc683cd4d65ceab8084daa80f41
George Tims <george.tims@hotmail.com>
2022-11-09 18:54:53 -0500

    Added Subscription serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from feedaggregator.models import Bookmark, Entry
+from feedaggregator.models import Bookmark, Entry, Feed, Subscription
 from users.models import Subscriber
+from datetime import datetime, timezone
 
 class BookmarkSerializer(serializers.ModelSerializer):
     entry = serializers.PrimaryKeyRelatedField(many=False, queryset=Entry.objects.all())
@@ -15,4 +16,20 @@ class BookmarkSerializer(serializers.ModelSerializer):
         return Bookmark.objects.create(
             entry=validated_data['entry'],
             subscriber=validated_data['subscriber']
+        )
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(many=False, queryset=Subscriber.objects.all())
+    feed = serializers.PrimaryKeyRelatedField(many=False, queryset=Feed.objects.all())
+
+    class Meta:
+        model = Subscription
+        fields = ['id', 'user', 'feed']
+        depth = 1
+    
+    def create(self, validated_data):
+        return Subscription.objects.create(
+            user=validated_data['user'],
+            feed=validated_data['feed'],
+            date_subscribed=datetime.now(timezone.utc)
         )
