d65b3eb82e2080c491fb1dafdb78657220c7d4d8
George Tims <george.tims@hotmail.com>
2022-11-13 16:03:37 -0500

    Got adding subscription to work with AJAX"

--- feedaggregator/models.py
+++ feedaggregator/models.py
@@ -107,7 +107,7 @@ class Entry(models.Model):
 class Subscription(models.Model):
   user = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
   feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
-  date_subscribed = models.DateTimeField()
+  date_subscribed = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.feed.title
