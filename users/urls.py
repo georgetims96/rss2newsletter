97dbd6c7207b8a2ce2de959910dfe7d01ba527b3
George Tims <george.tims@hotmail.com>
2022-12-15 16:09:34 -0500

    Updated template and urls for guest login

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from users.views import RegisterCreateView, LoginView, LogoutView
+from users.views import RegisterCreateView, LoginView, LogoutView, GuestLoginRedirectView
 
 app_name = 'users'
 
 urlpatterns = [
   path('register/', RegisterCreateView.as_view(), name='register'),
   path('login/', LoginView.as_view(), name='login'),
-  path('logout', LogoutView.as_view(), name='logout')
+  path('logout', LogoutView.as_view(), name='logout'),
+  path('guest_login/', GuestLoginRedirectView.as_view(), name='guest_login')
 ]
