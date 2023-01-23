dafce9deabba40d53e0e39c83e973e31c2668cc3
George Tims <george.tims@hotmail.com>
2023-01-22 21:37:01 -0500

    Moved dev environmental variables to file

--- rss2newsletters/settings.py
+++ rss2newsletters/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1th@fzd3jwm1u@y+m24d)lfkh#*%x$drjeyi0-!nu5mdv!kk5^'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 0))
 
 ALLOWED_HOSTS = []
 
